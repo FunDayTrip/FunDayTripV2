308fb0ad658aec0175cb02a292f035a44cea0b22
Verna <k6886wns@hotmail.com>
2020-09-17 10:41:29 +0800

    2020/9/17/9:17
    FIRST HOMEPAGE

---
+++ homepage/homepage/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿
+using homepage.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+namespace homepage.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      
+       
+   
+        // GET: Home
+        public ActionResult Index()
+        {
+ 
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(CAddLocationViewModel locate)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "INSERT INTO tTransaction (fDate) VALUES ('"+locate.txtLocationName+"')";
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            return View();
+        }
+
+       
+    }
+}
---
+++ homepage/homepage/ViewModel/CAddLocationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace homepage.ViewModel
+{
+    public class CAddLocationViewModel
+    {
+        public string txtLocationName { get; set; }
+    }
+}
