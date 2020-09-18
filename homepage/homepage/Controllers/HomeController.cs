87dccd69f4b6cad34c33ac4c06ea54d10210daf1
Verna <k6886wns@hotmail.com>
2020-09-18 16:01:28 +0800

    20200918
    location_version1

--- homepage/homepage/Controllers/HomeController.cs
+++ homepage/homepage/Controllers/HomeController.cs
@@ -14,17 +14,11 @@ namespace homepage.Controllers
     public class HomeController : Controller
     {
 
-      
-       
-   
+
+
+
         // GET: Home
         public ActionResult Index()
-        {
- 
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Index(CAddLocationViewModel locate)
         {
             SqlConnection con = new SqlConnection();
             con.ConnectionString = @"Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
@@ -32,7 +26,36 @@ namespace homepage.Controllers
 
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = con;
-            cmd.CommandText = "INSERT INTO tTransaction (fDate) VALUES ('"+locate.txtLocationName+"')";
+            string sql = "SELECT fDate FROM tTransaction WHERE fId = 2005";
+            cmd.CommandText = sql;
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            CAddLocationViewModel locationViewModel = new CAddLocationViewModel();
+            while (reader.Read())
+            {
+
+                ViewBag.kk = reader["fDate"].ToString();
+            }
+
+            con.Close();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(CAddLocationViewModel locate)
+        {
+            string photoname = Guid.NewGuid().ToString(); //重新命名一個不會重複的照片ID進資料庫
+            photoname += Path.GetExtension(locate.image.FileName);//取得副檔名
+            locate.image.SaveAs(Server.MapPath("~/Content/" + photoname)); //根目錄:~(不行),要用..回上一層
+            locate.fImage = "../Content/" + photoname;
+
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "INSERT INTO tTransaction (fDate) VALUES ('" + locate.fImage + "')";
             cmd.ExecuteNonQuery();
 
             con.Close();
@@ -40,6 +63,6 @@ namespace homepage.Controllers
             return View();
         }
 
-       
+
     }
 }
