a597ea46c9fa820dd8b979535ab8d817ef29475a
skrabbit0000 <65816363+skrabbit0000@users.noreply.github.com>
2020-09-25 15:55:09 +0800

    聊天室功能(可互相聊天)

--- homepage/homepage/Controllers/HomeController.cs
+++ homepage/homepage/Controllers/HomeController.cs
@@ -25,24 +25,24 @@ namespace homepage.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = @"Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
-            con.Open();
+            //SqlConnection con = new SqlConnection();
+            //con.ConnectionString = @"Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
+            //con.Open();
 
-            SqlCommand cmd = new SqlCommand();
-            cmd.Connection = con;
-            string sql = "SELECT fDate FROM tTransaction WHERE fId = 2005";
-            cmd.CommandText = sql;
+            //SqlCommand cmd = new SqlCommand();
+            //cmd.Connection = con;
+            //string sql = "SELECT fDate FROM tTransaction WHERE fId = 2005";
+            //cmd.CommandText = sql;
 
-            SqlDataReader reader = cmd.ExecuteReader();
-            CAddLocationViewModel locationViewModel = new CAddLocationViewModel();
-            while (reader.Read())
-            {
+            //SqlDataReader reader = cmd.ExecuteReader();
+            //CAddLocationViewModel locationViewModel = new CAddLocationViewModel();
+            //while (reader.Read())
+            //{
 
-                ViewBag.kk = reader["fDate"].ToString();
-            }
+            //    ViewBag.kk = reader["fDate"].ToString();
+            //}
 
-            con.Close();
+            //con.Close();
             return View();
         }
         [HttpPost]
@@ -122,7 +122,7 @@ namespace homepage.Controllers
                 Session[CDictionary.SK_MemberId] = loginMember.fId_Member;
                 Session[CDictionary.SK_MemberLogin] = loginMember;
                 Session[CDictionary.SK_ActiveRoleId] = loginMember.fActiveRoleId_Member;
-                
+
             }
             else if (q.FirstOrDefault().fPassword_Member != pwd)
             {
@@ -152,7 +152,7 @@ namespace homepage.Controllers
                     select n;
 
             List<CNotes> note = new List<CNotes>();
-            foreach(var n in q.ToList())
+            foreach (var n in q.ToList())
             {
                 CNotes nc = new CNotes();
                 nc.fMessage_Note = n.fMessage_Note;
@@ -164,20 +164,55 @@ namespace homepage.Controllers
         [HttpPost]
         public JsonResult readRoles(string member_id)
         {
-            List<CRole> roles = new CRolesFactory().readRoles(member_id);          
-            
-            
-            return Json(roles);            
+            List<CRole> roles = new CRolesFactory().readRoles(member_id);
+
+
+            return Json(roles);
         }
         [HttpPost]
         public string changeRole(int role_id)
         {
             CRole rol = new CRolesFactory().getRole(role_id);
-            
+
             Session[CDictionary.SK_ActiveRoleId] = rol.fId_Role;
             Session[CDictionary.SK_ActiveRoleName] = rol.fNickName_Role;
             return rol.fNickName_Role;
         }
+        public JsonResult showfollow(int role_id)
+        {
+            var q = from p in dbFundaytrip.tFollows
+                    where p.fId_Self_Role == role_id
+                    select p;
 
+
+            List<CFollowlistViewModel> f = new List<CFollowlistViewModel>();
+            foreach (var x in q.ToList())
+            {
+                CFollowlistViewModel FV = new CFollowlistViewModel();
+                FV.follow_Self_Name = x.tRole.fNickName_Role;
+                FV.follow_Self_ID = x.tRole.fId_Role;
+                FV.follow_Target_Name = x.tRole1.fNickName_Role;
+                FV.follow_Target_ID = x.tRole1.fId_Role;
+                f.Add(FV);
+            }
+            return Json(f);
+        }
+        public void pushmessage(int fId_To_Role, int fId_From_Role, string message)
+        {
+           DB_FunDayTripEntities db =new DB_FunDayTripEntities();
+            tMessage CM = new tMessage();
+
+
+
+            CM.fId_To_Role = fId_To_Role;
+            CM.fId_From_Role = fId_From_Role;
+            CM.fMessage_Message = message;
+            CM.fTime_Message = DateTime.Now;
+
+            db.tMessages.Add(CM);
+
+            db.SaveChanges();
+        }
     }
+      
 }
---
+++ homepage/homepage/SignalR/MyHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace homepage.SignalR
+{
+    public class MyHub : Hub
+    {
+        public void Hello(string name,string content)
+        {
+            Clients.All.hello(name,content);
+        }
+    }
+}
