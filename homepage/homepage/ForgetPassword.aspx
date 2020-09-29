3dab0df5e6b6746161b3aaf4c3c20e0ce9e4ea35
john <adegame1230@gmail.com>
2020-09-29 18:39:20 +0800

    忘記密碼

---
+++ homepage/homepage/ForgetPassword.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ForgetPassword.aspx.cs" Inherits="homepage.ForgetPassword" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <center>
+                請輸入你的信箱:<asp:TextBox ID="Txtmail" runat="server"></asp:TextBox><br />
+                                <asp:Button ID="Butpwd" runat="server" Text="送出" OnClick="Butpwd_Click"></asp:Button><br />
+                                <asp:Label ID="Label" runat="server" ></asp:Label>
+            </center>
+        </div>
+    </form>
+</body>
+</html>
---
+++ homepage/homepage/ForgetPassword.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace homepage
+{
+    public partial class ForgetPassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Butpwd_Click(object sender, EventArgs e)
+        {
+            DB_FunDayTripEntities dbFundaytrip = new DB_FunDayTripEntities();
+            var q = from m in dbFundaytrip.tMembers
+                    where m.fEmail_Member == Txtmail.Text
+                    select m;
+            if (!q.Any())
+            {
+                Label.Text = "無此帳號";
+            }
+            else
+            {
+            //MailMessage mm = new MailMessage("msit1271230@gmail.com", Txtmail.Text);
+            //mm.Subject = "驗證碼!";
+            //mm.Body = "你好 請妥善保管你的密碼";
+            //mm.IsBodyHtml = true;
+            //SmtpClient smtp = new SmtpClient();
+            //smtp.Host = "smtp.gmail.com";
+            //smtp.EnableSsl = true;
+            //NetworkCredential nc = new NetworkCredential();
+            //nc.UserName = "msit1271230@gmail.com";
+            //nc.Password = "gijoe1230";
+            //smtp.UseDefaultCredentials = true;
+            //smtp.Credentials = nc;
+            //smtp.Port = 587;
+            //smtp.Send(mm);
+                Label.Text = "已送出請檢查信箱";
+            }
+            
+
+        }
+    }
+}
