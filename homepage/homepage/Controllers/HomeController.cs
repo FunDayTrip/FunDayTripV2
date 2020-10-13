db23d208ae06536a1347cc3b4e7de52e085bc542
skrabbit0000 <65816363+skrabbit0000@users.noreply.github.com>
2020-10-13 14:00:42 +0800

    TSENG1013

--- homepage/homepage/Controllers/HomeController.cs
+++ homepage/homepage/Controllers/HomeController.cs
@@ -1550,6 +1550,7 @@ namespace homepage.Controllers
                     where myID == q.fId_Role 
                     select z).FirstOrDefault();
             //自己
+
             var q1 = (from p1 in dbFundaytrip.tFollows
                       where p1.fId_Target_Role == q.fId_Role && p1.fId_Self_Role != q.fId_Role
                       select p1).FirstOrDefault();
@@ -1558,7 +1559,7 @@ namespace homepage.Controllers
             if (g != null)
                 return "自己";
             if (q1 != null)
-                return "追隨中";
+                return "已追隨";
             else
                 return "追隨";
         
@@ -1577,7 +1578,22 @@ namespace homepage.Controllers
                 db.tFollows.Add(tf);
                 db.SaveChanges();
 
-                return "追隨中";
+                return "已追隨";
+            }
+            if (statestring == "已追隨")
+            {
+                var q = (from p in dbFundaytrip.tLocations
+                         where p.fId_Location == Loction_Id
+                         select p).FirstOrDefault();
+                tFollow tf = new tFollow();
+                tf.fId_Target_Role = q.fId_Role;
+                tf.fId_Self_Role = myID;
+                var q1 = (from p1 in dbFundaytrip.tFollows
+                          where p1.fId_Self_Role == tf.fId_Self_Role && p1.fId_Target_Role == tf.fId_Target_Role
+                          select p1).FirstOrDefault();
+                dbFundaytrip.tFollows.Remove(q1);
+                dbFundaytrip.SaveChanges();
+                return "追隨";
             }
             else
                 return statestring;
